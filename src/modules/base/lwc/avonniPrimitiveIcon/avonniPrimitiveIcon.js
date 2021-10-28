d9ddcd8b5b82d9ec9dbd2f8f8111867dae9326b9
Aliaksandr Antaniuk <aleksandrantonyuk89@gmail.com>
2021-10-28 22:56:44 +0300

    Updated version 0.0.16

--- src/modules/base/lwc/avonniPrimitiveIcon/avonniPrimitiveIcon.js
+++ src/modules/base/lwc/avonniPrimitiveIcon/avonniPrimitiveIcon.js
@@ -33,41 +33,96 @@
 import { LightningElement, api } from 'lwc';
 import { classSet } from 'c/utils';
 import { normalizeString as normalize } from 'c/utilsPrivate';
-import * as iconUtils from 'c/iconUtils';
 import standardTemplate from './avonniPrimitiveIcon.html';
-import { getIconSvgTemplates } from 'c/configProvider';
+import { getName, isValidName, polyfill } from 'c/iconUtils';
+import { fetchIconLibrary, hasIconLibrary, getIconLibrary } from './avonniFetch';
 
 const ICON_SIZES = {valid:['xx-small', 'x-small', 'small', 'medium', 'large'] , default: 'medium'}
-
 const ICON_VARIANTS = {valid: ['bare', 'error', 'inverse', 'warning', 'success'], default: ''}
 
 export default class AvonniPrimitiveIcon extends LightningElement {
-    @api iconName;
     @api src;
     @api svgClass;
     @api size = ICON_SIZES.default;
     @api variant;
 
-    privateIconSvgTemplates = getIconSvgTemplates();
+    iconLibrary = null;
+    _iconName = null;
 
-    get inlineSvgProvided() {
-        return !!this.privateIconSvgTemplates;
+    @api
+    get iconName() {
+        return this._iconName;
     }
-
-    renderedCallback() {
-        if (this.iconName !== this.prevIconName && !this.inlineSvgProvided) {
-            this.prevIconName = this.iconName;
-            const svgElement = this.template.querySelector('[data-element-id="svg"]');
-            iconUtils.polyfill(svgElement);
+    set iconName(value) {
+        if (value !== this._iconName) {
+            this._iconName = value;
+            this.requestIconTemplates();
         }
     }
 
+    get category() {
+        if (isValidName(this._iconName)) {
+            const [spriteName] = this._iconName.split(':');
+            return spriteName;
+        }
+        return null;
+    }
+
+    get isReady() {
+        return !!this.iconLibrary;
+    }
+
+    // eslint-disable-next-line @lwc/lwc/no-async-await
+    async requestIconTemplates() {
+        if (hasIconLibrary('ltr', this.category)) {
+            this.iconLibrary = getIconLibrary('ltr', this.category);
+            return;
+        }
+
+        if (this.category) {
+            try {
+                this.iconLibrary = null;
+                this.iconLibrary = await fetchIconLibrary('ltr', this.category);
+            } catch (e) {
+                // eslint-disable-next-line no-console
+                console.warn(
+                    `<avonni-primitive-icon> failed to dynamically import icon templates for ${this.category}: ${e.message}`
+                );
+            }
+        }
+    }
+
+    renderedCallback() {
+        if (this.isReady || this.iconName !== this.prevIconName) {
+            this.prevIconName = this.iconName;
+            const svgElement = this.template.querySelector('svg');
+            polyfill(svgElement);
+        }
+    }
+
+    render() {
+        if (this.isReady) {
+            if (!this.src) {
+                const name = this.iconName;
+                if (isValidName(name)) {
+                    const [spriteName, iconName] = name.split(':');
+                    const template =
+                        this.iconLibrary[`${spriteName}_${iconName}`];
+                    if (template) {
+                        return template;
+                    }
+                }
+            }
+        }
+        return standardTemplate;
+    }
+
     get href() {
-        return this.src || iconUtils.getIconPath(this.iconName, '');
+        return this.src || '';
     }
 
     get name() {
-        return iconUtils.getName(this.iconName);
+        return getName(this.iconName);
     }
 
     get normalizedSize() {
@@ -116,27 +171,5 @@ export default class AvonniPrimitiveIcon extends LightningElement {
         }
 
         return classes.toString();
-    }
-
-    resolveTemplate() {
-        const name = this.iconName;
-        if (iconUtils.isValidName(name)) {
-            const [spriteName, iconName] = name.split(':');
-            const template = this.privateIconSvgTemplates[
-                `${spriteName}_${iconName}`
-            ];
-
-            if (template) {
-                return template;
-            }
-        }
-        return standardTemplate;
-    }
-
-    render() {
-        if (this.inlineSvgProvided) {
-            return this.resolveTemplate();
-        }
-        return standardTemplate;
     }
 }
